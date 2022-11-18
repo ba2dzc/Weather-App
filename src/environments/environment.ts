a8a8e95589909a264798e84f7170f2f638de6c68
ba2dzc <batu.duzcu@gmail.com>
2022-11-18 13:44:52 +0300

    API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiBaseUrl:'https://yahoo-weather5.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderName:'X-RapidAPI-Key',
+  XRapidAPIHostHeaderValue:'yahoo-weather5.p.rapidapi.com',
+  XRapidAPIKeyHeaderName:'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue:' ### Api Key Value ### '
 };
 
 /*
